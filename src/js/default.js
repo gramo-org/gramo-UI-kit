853d0033763982f1df76161224c07285b6f6abca
Fred Lesser <Freds@Freds-MacBook-Pro.local>
2015-12-22 13:59:43 +0100

    Add basic program admin page

--- src/js/default.js
+++ src/js/default.js
@@ -1,11 +1,24 @@
-// Default JavaScript Functions and Initiations
+'use strict';
 $(document).ready(function() {
-
+  // Toggle sidebar
   $('.toggle-sidebar').click(function(e) {
     e.preventDefault();
     $(this).toggleClass('open');
     $('.sidebar').toggleClass('sidebar--isOpen');
     $('main').toggleClass('push');
+  });
+
+
+  //Fix contentHeader to top of viewport when scrolling
+  var contentHeader = $('.content-header--fixed');
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 50) {
+      contentHeader.css('top', '0');
+    } else {
+      contentHeader.css('top', 'auto');
+    }
   });
 
 }); // end document ready
