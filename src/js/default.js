283c400b0e6a6671ff8c7b2af72785b265a23609
Fred Lesser <Freds@Freds-MacBook-Pro.local>
2016-01-07 12:57:22 +0100

    Change '&__nav__item--active to '.active'

--- src/js/default.js
+++ src/js/default.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
     // If no match is found, use the first link as the initial active tab.
     $active = $($links.filter('[href="' + location.hash + '"]')[0] ||
       $links[0]);
-    $active.parent().addClass('tabs__nav__item--active');
+    $active.addClass('active');
 
     $content = $($active[0].hash);
 
@@ -42,7 +42,7 @@ $(document).ready(function() {
     // Bind the click event handler
     $(this).on('click', 'a', function(e) {
       // Make the old tab inactive.
-      $active.parent().removeClass('tabs__nav__item--active');
+      $active.removeClass('active');
       $content.hide();
 
       // Update the variables with the new link and content
@@ -50,7 +50,7 @@ $(document).ready(function() {
       $content = $(this.hash);
 
       // Make the tab active.
-      $active.parent().addClass('tabs__nav__item--active');
+      $active.addClass('active');
       $content.show();
 
       // Prevent the anchor's default click action
