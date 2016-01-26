1f9edcc5b133d5fee7f05a602348ddf0103efad5
Fred Lesser <Freds@Freds-MacBook-Pro.local>
2016-01-26 10:53:36 +0100

    Fix mobile styles for hover actions in table row

--- src/js/default.js
+++ src/js/default.js
@@ -63,12 +63,4 @@ $(document).ready(function() {
     '<div class="btn-group btn-group--hover"><button class="btn btn--success btn--small">Kommenter</button><button class="btn btn--success btn--small">Ignorer</button></div>';
   $('td.action').append(buttons);
 
-  // $('.table tbody tr').hover(function() {
-  //   $(this).addClass('table__row--isEditing');
-  //   $(this).find('.btn-group--hover').addClass('show');
-  // }, function() {
-  //   $(this).removeClass('table__row--isEditing');
-  //   $(this).find('.btn-group--hover').removeClass('show');
-  //
-  // });
 }); // end document ready
