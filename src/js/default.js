c6746bd0c4b98cd20ae1c50e9c62c14fffc58753
Fred Lesser <Freds@Freds-MacBook-Pro.local>
2016-04-18 14:04:38 +0200

    Give flash-message and site-header fixed position

--- src/js/default.js
+++ src/js/default.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     var scroll = $(window).scrollTop();
 
     if (scroll >= 50) {
-      contentHeader.css('top', '0');
+      contentHeader.css('top', '3rem');
     } else {
       contentHeader.css('top', 'auto');
     }
