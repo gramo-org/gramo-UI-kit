fb32b4907942c36bf4157b99b69375462a22c402
Fred Lesser <fredericklesser@gmail.com>
2017-04-03 16:25:50 +0200

    Add file upload styles

--- src/js/default.js
+++ src/js/default.js
@@ -148,7 +148,7 @@ $(document).ready(function() {
   var headerHeight = $(".toggle-sidebar").height();
 
   // Attach the click event
-  $('a[href*=#]').bind("click", function(e) {
+  $('.vertical__nav--sub__item > a[href*=#]').bind("click", function(e) {
       e.preventDefault();
 
       var target = $(this).attr("href"); //Get the target
@@ -202,6 +202,24 @@ $(document).ready(function() {
      $('h4:contains("Markup")').click(function(e){
        e.preventDefault();
        $(this).next('.highlight').toggleClass('is-visible')
+     })
+     
+     $('.file-upload__toggle').click(function(e){
+       e.preventDefault();
+       $(this).next('.file-upload__inner').toggleClass('is-visible');
+     })
+     
+     $('.file-upload .form__section--action .btn').click(function(e){
+       e.preventDefault();
+       $(this).closest('.form').hide();
+       $(this).closest('.form').next('.file-upload__list').show();
+     })
+     
+     $('.done').click(function(e){
+       e.preventDefault();
+       $(this).closest('.file-upload__list').hide();
+       $(this).parent().prev('.form').show();
+       $(this).parent().parent().removeClass('is-visible');
      })
 
 
