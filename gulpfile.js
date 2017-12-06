b3097381f6e872d6eb4d76a2c7e43e8240f3e61e
Trym Skaar <trym.skaar@gmail.com>
2017-12-06 15:00:06 +0100

    Disable svg processing
    Hackaround for issues on Fred's computer.

--- gulpfile.js
+++ gulpfile.js
@@ -1,107 +1,124 @@
 // Base Gulp File
-var gulp = require('gulp'),
-  watch = require('gulp-watch'),
-  path = require('path'),
-  postcss = require('gulp-postcss'),
-  inlinesource = require('gulp-inline-source'),
-  browserSync = require('browser-sync'),
-  imagemin = require('gulp-imagemin'),
-  cache = require('gulp-cache'),
-  uglify = require('gulp-uglify'),
-  runSequence = require('run-sequence');
+var gulp = require("gulp"),
+  watch = require("gulp-watch"),
+  path = require("path"),
+  postcss = require("gulp-postcss"),
+  inlinesource = require("gulp-inline-source"),
+  browserSync = require("browser-sync"),
+  imagemin = require("gulp-imagemin"),
+  cache = require("gulp-cache"),
+  uglify = require("gulp-uglify"),
+  runSequence = require("run-sequence");
 
 // Task to compile SCSS
-gulp.task('postcss', function() {
-  var autoprefixer = require('autoprefixer'),
-    pImport = require('postcss-import'),
-    customProps = require('postcss-custom-properties'),
-    customMedia = require('postcss-custom-media'),
-    comments = require('postcss-discard-comments'),
-    color = require('postcss-color-function'),
-    nested = require('postcss-nested'),
-    simpleExtend = require('postcss-extend');
+gulp.task("postcss", function() {
+  var autoprefixer = require("autoprefixer"),
+    pImport = require("postcss-import"),
+    customProps = require("postcss-custom-properties"),
+    customMedia = require("postcss-custom-media"),
+    comments = require("postcss-discard-comments"),
+    color = require("postcss-color-function"),
+    nested = require("postcss-nested"),
+    simpleExtend = require("postcss-extend");
 
-  return gulp.src(['./src/css/style.css','./src/css/minside.css'], {base: './src/css/'})
-    .pipe(postcss([
-      pImport(),
-      nested(),
-      simpleExtend(),
-      comments({
-        discardAll: true
-      }),
-      customProps(),
-      customMedia(),
-      color(),
-      autoprefixer({
-        browsers: ['last 6 versions'],
-        cascade: false,
-      }),
-    ]))
-    .pipe(gulp.dest('./src/'))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
+  return gulp
+    .src(["./src/css/style.css", "./src/css/minside.css"], {base: "./src/css/"})
+    .pipe(
+      postcss([
+        pImport(),
+        nested(),
+        simpleExtend(),
+        comments({
+          discardAll: true,
+        }),
+        customProps(),
+        customMedia(),
+        color(),
+        autoprefixer({
+          browsers: ["last 6 versions"],
+          cascade: false,
+        }),
+      ])
+    )
+    .pipe(gulp.dest("./src/"))
+    .pipe(gulp.dest("./dist/"))
+    .pipe(
+      browserSync.reload({
+        stream: true,
+      })
+    );
 });
 
 // Task to move compiled CSS to `dist` folder
-gulp.task('movecss', function() {
-  return gulp.src('./src/style.css')
-    .pipe(gulp.dest('./dist/'));
+gulp.task("movecss", function() {
+  return gulp.src("./src/style.css").pipe(gulp.dest("./dist/"));
 });
 
 // Task to move compiled CSS to `dist` folder
-gulp.task('movefonts', function() {
-  return gulp.src('./src/fonts/*.*')
-    .pipe(gulp.dest('./dist/fonts'));
+gulp.task("movefonts", function() {
+  return gulp.src("./src/fonts/*.*").pipe(gulp.dest("./dist/fonts"));
 });
 
 // Task to Minify JS
-gulp.task('jsmin', function() {
-  return gulp.src('./src/js/**/*.js')
+gulp.task("jsmin", function() {
+  return gulp
+    .src("./src/js/**/*.js")
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/js/'));
+    .pipe(gulp.dest("./dist/js/"));
 });
 
 // Minify Images
-gulp.task('imagemin', function() {
-  return gulp.src('./src/img/**/*.+(png|jpg|jpeg|gif|svg)')
-    // Caching images that ran through imagemin
-    .pipe(cache(imagemin({
-      interlaced: true
-    })))
-    .pipe(gulp.dest('./dist/img'));
+gulp.task("imagemin", function() {
+  return (gulp
+      .src("./src/img/**/*.+(png|jpg|jpeg|gif)")
+      // Caching images that ran through imagemin
+      .pipe(
+        cache(
+          imagemin({
+            interlaced: true,
+          })
+        )
+      )
+      .pipe(gulp.dest("./dist/img")) );
 });
 
 // BrowserSync Task (Live reload)
-gulp.task('browserSync', function() {
+gulp.task("browserSync", function() {
   browserSync({
     server: {
-      baseDir: './src/'
+      baseDir: "./src/",
     },
-  })
+  });
 });
 
 // Gulp Inline Source Task
 // Embed scripts, CSS or images inline (make sure to add an inline attribute to the linked files)
 // Eg: <script src="default.js" inline></script>
 // Will compile all inline within the html file (less http requests - woot!)
-gulp.task('inlinesource', function() {
-  return gulp.src('./src/**/*.html')
+gulp.task("inlinesource", function() {
+  return gulp
+    .src("./src/**/*.html")
     .pipe(inlinesource())
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest("./dist/"));
 });
 
 // Gulp Watch Task
-gulp.task('watch', ['browserSync'], function() {
-  gulp.watch('./src/css/**/*', ['postcss']);
-  gulp.watch('./src/**/*.html').on('change', browserSync.reload);
+gulp.task("watch", ["browserSync"], function() {
+  gulp.watch("./src/css/**/*", ["postcss"]);
+  gulp.watch("./src/**/*.html").on("change", browserSync.reload);
 });
 
 // Gulp Default Task
-gulp.task('default', ['watch']);
+gulp.task("default", ["watch"]);
 
 // Gulp Build Task
-gulp.task('build', function() {
-  runSequence('postcss', 'movecss', 'movefonts', 'imagemin', 'jsmin', 'inlinesource');
+gulp.task("build", function() {
+  runSequence(
+    "postcss",
+    "movecss",
+    "movefonts",
+    "imagemin",
+    "jsmin",
+    "inlinesource"
+  );
 });
